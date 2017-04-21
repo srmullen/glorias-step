30d9d65ac3daf25e4c94fe8ad7aba7437a4cace7
Sean Mullen <srmullen13@gmail.com>
2017-04-21 15:17:08 -0600

    built with boethius-server

---
+++ temp.js
@@ -0,0 +1,6 @@
+import gloriasStepLayout from "./src/glorias-step-layout.json";
+import gloriasStep from "./src/glorias-step.bth";
+import starspangledbanner from "./src/starspangledbanner.bth";
+
+export const musics = {gloriasStep,starspangledbanner,};
+export const layouts = {gloriasStepLayout,};
